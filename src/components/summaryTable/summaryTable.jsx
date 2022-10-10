aeddf24a95a30a714a0c8f8c67f3960d0c762781
Oksana Stets <oksanastets73@gmail.com>
2022-10-10 23:19:31 +0300

    complete summary

--- src/components/summaryTable/summaryTable.jsx
+++ src/components/summaryTable/summaryTable.jsx
@@ -1,52 +1,47 @@
 import { useState, useEffect } from "react";
-import InitialSum from "../../sum.json"
+import InitialSum from "../../sum.json";
 
-import { getUniqueOptions } from "../../helper/getUniqueOptions"
+import { getUniqueOptions } from "../../helper/getUniqueOptions";
 
 export const SummaryTable = (arr) => {
-    const [summaryData, setSummaryData] = useState(InitialSum);
-    // const [notes, setNotes] = useState([]);
-    // const [options, setOptions] = useState([]);
-    const [summaryItem, setSummaryItem] = useState({
-        option: "",
-        active: null,
-        archived: null
-    });
-    // const notes = Object.values(arr)[0];
-    // console.log(notes);
-    // notes.forEach(note => console.log(note.status));
-    // const categories = getUniqueOptions(notes);
-    // console.log(categories);
-    useEffect(() => {
-        const notes = Object.values(arr)[0];
-        const categories = getUniqueOptions(notes);
-        
-     categories.forEach(category => {
-                console.log(category);
-                const arrayByCategory = notes.filter(note => note.category === category);
-                console.log(arrayByCategory);
-                const activeNotes = arrayByCategory.filter(note => note.status === "active")
-                console.log(activeNotes.length);
-                const archivedNotes = arrayByCategory.filter(note => note.status === "archived")
-                console.log(archivedNotes.length);
-       
-                setSummaryItem({
-                    option: category,
-                    active: activeNotes.length,
-                    archived: archivedNotes.length
-                });
-                // console.log(summaryItem);
-            })
-    }, [arr])
-//     useEffect(() => {
-//     setSummaryData(prev=> [...prev, ...summaryItem])
-//   }, [summaryItem]);
-//  setSummaryData(prev=> [...prev, ...summaryItem])
-    // console.log(summaryData);
-    
-   
+  const [summaryData, setSummaryData] = useState(InitialSum);
 
-    return (
+  //   const [summaryItem, setSummaryItem] = useState({
+  //       option: "",
+  //       active: null,
+  //       archived: null,
+  //   });
+
+  useEffect(() => {
+    setSummaryData([]);
+    const notes = Object.values(arr)[0];
+    const categories = getUniqueOptions(notes);
+
+    categories.forEach((category) => {
+      console.log(category);
+      const arrayByCategory = notes.filter(
+        (note) => note.category === category
+      );
+      console.log(arrayByCategory);
+      const activeNotes = arrayByCategory.filter(
+        (note) => note.status === "active"
+      );
+      console.log(activeNotes.length);
+      const archivedNotes = arrayByCategory.filter(
+        (note) => note.status === "archived"
+      );
+      console.log(archivedNotes.length);
+
+      const newSummaryRowData = {};
+      newSummaryRowData.option = category;
+      newSummaryRowData.active = activeNotes.length;
+      newSummaryRowData.archived = archivedNotes.length;
+
+      setSummaryData((prev) => [...prev, newSummaryRowData]);
+    });
+  }, [arr]);
+
+  return (
     <tbody>
       {summaryData.map((data) => (
         <tr key={data.option}>
