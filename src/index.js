a280d7a12ce95f2d0e27f59bde911028815c0171
Oksana Stets <oksanastets73@gmail.com>
2022-10-16 00:27:20 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,16 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import { Provider } from 'react-redux'
-// import store from './store'
+import store from './redux/store'
 import './index.css';
 import App from './App';
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <Provider>
+  <Provider store={store}>
     <React.StrictMode>
     <App />
     </React.StrictMode>
-  // </Provider>
+  </Provider>
 );
