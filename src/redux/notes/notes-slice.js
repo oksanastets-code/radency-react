7602625acdfca167d2056a6bde94d453b7ac46aa
Oksana Stets <oksanastets73@gmail.com>
2022-10-19 00:13:02 +0300

    Update notes-slice.js

--- src/redux/notes/notes-slice.js
+++ src/redux/notes/notes-slice.js
@@ -1,17 +1,42 @@
-import { createSlice } from '@reduxjs/toolkit'
-import initialState from "../../notes.json"
+import { createSlice } from "@reduxjs/toolkit";
+import initialState from "../../notes.json";
 
 export const notesSlice = createSlice({
-  name: 'notes',
+  name: "notes",
   initialState,
   reducers: {
     addNote: (state, action) => {
       state.push(action.payload);
       return state;
+    },
+    deleteNote: (state, action) => {
+      return state.filter(({ id }) => id !== action.payload);
+    },
+    editNote: (state, action) => {
+      return state.map((note) => {
+        if (note.id === action.payload.id) {
+          return {
+            ...note,
+            item: action.payload.item,
+          };
+        }
+        return note;
+      });
+    },
+    archiveNote: (state, action) => {
+      return state.map((note) => {
+        if (note.id === action.payload) {
+          return {
+            ...note,
+            status: "archived"
+          }
+        }
+        return note;
+      })
     }
   },
-})
+});
 
-export const { addNote } = notesSlice.actions;
+export const { addNote, deleteNote, editNote, archiveNote } = notesSlice.actions;
 
-export default notesSlice.reducer
+export default notesSlice.reducer;
